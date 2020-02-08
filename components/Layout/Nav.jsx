85a74fcb70148cf4634ff97fd35d49cd8e3d5009
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 16:07:27 -0500

    style active

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
+import cn from 'classnames';
 
 import styles from './layout.module.scss';
-
-const links = [
-  { href: '/contact', label: 'Contact' },
-  { href: '/resume', label: 'Resume' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -23,21 +16,35 @@ Router.onRouteChangeError = () => {
   NProgress.done();
 };
 
-const Nav = () => (
-  <nav className={styles.navbar}>
-    <Link href="/">
-      <a className={styles.homeLink}>Valmassoi</a>
-    </Link>
-    <ul className={styles.navLinks}>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <Link href={href}>
-            <a>{label}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Nav = () => {
+  const links = [
+    { href: '/resume', label: 'Resume' },
+    { href: '/contact', label: 'Contact' },
+  ].map(link => {
+    const router = useRouter();
+    link.key = `nav-link-${link.href}-${link.label}`;
+    link.active = router.pathname === link.href;
+    return link;
+  });
+
+  console.log(links)
+
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <a className={styles.homeLink}>Valmassoi</a>
+      </Link>
+      <ul className={styles.navLinks}>
+        {links.map(({ key, href, label, active }) => (
+          <li key={key} className={cn({[`${styles.activeLink}`]: active})}>
+            <Link href={href}>
+              <a>{label}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
