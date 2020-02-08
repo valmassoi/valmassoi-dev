355d706c6518eb68cb622af1317917502e676990
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 18:52:55 -0500

    not found

---
+++ pages/_error.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Layout from '../components/Layout/Layout';
+import NotFound from '../components/svgs/notFound.svg'
+
+import styles from './error.module.scss'
+
+const Error = () => {
+  return (
+    <Layout>
+      <div className={styles.notFoundWrapper}>
+        <NotFound />
+      </div>
+    </Layout>
+  );
+};
+
+// Error.getInitialProps = ({ res, err }) => {
+//   const statusCode = (res && res.statusCode) || (err && err.statusCode) || 404;
+//   return { statusCode };
+// };
+
+// Error.propTypes = {
+//   statusCode: PropTypes.number.isRequired,
+// };
+
+export default Error;
