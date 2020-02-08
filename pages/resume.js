a55a0061b971d666f16795bbb4385f2cc7f75283
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 17:24:39 -0500

    fetch resume

--- pages/resume.js
+++ pages/resume.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/Layout/Layout';
-import { H1 } from '../components/htmlElements';
-
+// import { H1 } from '../components/htmlElements';
 import styles from './resume.module.scss';
 
-const Resume = () => {
+const fetchResumeData = async () => {
+  const res = fetch(`https://raw.githubusercontent.com/valmassoi/resume/master/Valmassoi_Resume_Soft-Dev.md`);
+  try {
+    return await res.then(response => response.text());
+  } catch (err) {
+    console.error(err);
+    return 'Error loading resume.'
+  }
+}
+
+const Resume = ({
+  resumeData,
+}) => {
   return (
     <Layout title="Resume">
-      <H1>Resume</H1>
+      <ReactMarkdown source={resumeData} className={styles.markdown} />
     </Layout>
   );
+};
+
+
+Resume.getInitialProps = async () => {
+  // fetch server-side
+  const resumeData = await fetchResumeData();
+  return { resumeData };
+};
+
+Resume.propTypes = {
+  resumeData: PropTypes.string.isRequired,
 };
 
 export default Resume;
