bca6a0af9caa8f7bb0d9575f63111e325fa2823a
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 18:00:18 -0500

    footer icon

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 import styles from './layout.module.scss';
 
 const Footer = () => (
   <div className={styles.footer}>
-    <a href="https://github.com/valmassoi" target="_blank" rel="noopener noreferrer" title="GitHub">GitHub</a>
+    <div className={styles.footerLinks}>
+      <a href="https://github.com/valmassoi" target="_blank" rel="noopener noreferrer" title="GitHub">
+        <FaGithub size="30px" />
+      </a>
+    </div>
   </div>
 );
 
