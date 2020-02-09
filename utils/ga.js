5ea90af55ab8ff3e0e7005d33769e4e62946266f
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 20:28:12 -0500

    fix dev ga

--- utils/ga.js
+++ utils/ga.js
@@ -2,16 +2,20 @@ export const GA_TRACKING_ID = 'UA-158072901-1';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  })
+  if (window.gtag) { // only avail in prod
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  })
+  if (window.gtag) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    })
+  }
 }
