7e576da45374fe5a533cb979e6ecc7bb61047c59
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-09 11:32:57 -0500

    init <Terminal />

---
+++ components/Terminal/Terminal.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { string, node } from 'prop-types';
+import cn from 'classnames';
+
+import styles from './terminal.module.scss';
+
+const Circle = ({ color, radius = 7 }) => (
+  <svg viewBox={`0 0 ${radius * 2} ${radius * 2}`} width={radius * 2} xmlns="http://www.w3.org/2000/svg">
+    <circle cx={radius} cy={radius} r={radius} fill={color} />
+  </svg>
+)
+
+const Terminal = ({ children, className, ...rest }) => {
+  return (
+    <div
+      className={cn(styles.terminal, className)}
+      {...rest}
+    >
+      <div className={styles.topBar}>
+        <Circle color="#ed645a" />
+        <Circle color="#e1c14c" />
+        <Circle color="#70bd3c" />
+      </div>
+      <div className={styles.terminalBody}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Terminal.propTypes = {
+  children: node.isRequired,
+  className: string,
+};
+
+Terminal.defaultProps = {
+  className: '',
+};
+
+export default Terminal;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typist from 'react-typist';
 
 import Layout from '../components/Layout/Layout';
+import Terminal from '../components/Terminal/Terminal';
 import { H1 } from '../components/htmlElements';
 
 import styles from './index.module.scss';
@@ -14,16 +15,18 @@ const Home = () => {
   return (
     <Layout title="Home">
       <div className={styles.page}>
-        <Typist
-          avgTypingDelay={50}
-          startDelay={2000}
-        >
-          <a>Hello World</a>
-          <Typist.Backspace count={5} delay={1000} />
-          <Typist.Delay ms={750} />
-          <a>I'm Rob</a>
-          {/* <p>A software developer</p> */}
-        </Typist>
+        <Terminal>
+          <Typist
+            avgTypingDelay={50}
+            startDelay={2000}
+          >
+            <a>Hello World</a>
+            <Typist.Backspace count={5} delay={1000} />
+            <Typist.Delay ms={750} />
+            <a>I'm Rob</a>
+            {/* <p>A software developer</p> */}
+          </Typist>
+        </Terminal>
       </div>
     </Layout>
   );
