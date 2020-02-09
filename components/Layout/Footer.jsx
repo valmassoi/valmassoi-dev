b6491d43a5370f0cb655a7698be96d29202a7c25
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-08 19:05:02 -0500

    add linkedin, footer colors

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { FaGithub, FaHackerrank } from 'react-icons/fa';
+import { FaGithub, FaHackerrank, FaLinkedin } from 'react-icons/fa';
 
-import styles from './layout.module.scss';
+import styles from './footer.module.scss';
 
-// TODO hover color of company
 const Footer = () => (
   <div className={styles.footer}>
     <div className={styles.footerLinks}>
-      <a href="https://github.com/valmassoi" target="_blank" rel="noopener noreferrer" title="GitHub">
+      <a className={styles.gitHub} href="https://github.com/valmassoi" target="_blank" rel="noopener noreferrer" title="GitHub">
         <FaGithub size="30px" />
       </a>
-      <a href="https://hackerrank.com/valmassoi" target="_blank" rel="noopener noreferrer" title="HackerRank">
+      <a className={styles.linkedIn} href="https://www.linkedin.com/in/robertvalmassoi" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+        <FaLinkedin size="30px" />
+      </a>
+      <a className={styles.hackerRank} href="https://hackerrank.com/valmassoi" target="_blank" rel="noopener noreferrer" title="HackerRank">
         <FaHackerrank size="30px" />
       </a>
     </div>
