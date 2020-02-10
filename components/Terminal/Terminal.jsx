6e60c64990fd046e567732f095d1fb38e55a3835
Robert Valmassoi <valmassoi@protonmail.com>
2020-02-10 09:49:07 -0500

    add icons to traffic lights

--- components/Terminal/Terminal.jsx
+++ components/Terminal/Terminal.jsx
@@ -6,6 +6,7 @@ import {
   func,
 } from 'prop-types';
 import cn from 'classnames';
+import { FaTimes, FaMinus, FaPlus } from 'react-icons/fa';
 
 import styles from './terminal.module.scss';
 
@@ -14,10 +15,14 @@ const Circle = ({
   radius,
   className,
   onClick,
+  icon,
 }) => (
-  <svg className={className} onClick={onClick} viewBox={`0 0 ${radius * 2} ${radius * 2}`} width={radius * 2} xmlns="http://www.w3.org/2000/svg">
-    <circle cx={radius} cy={radius} r={radius} fill={color} />
-  </svg>
+  <div className={styles.trafficLight} onClick={onClick} role="button">
+    <svg className={className} viewBox={`0 0 ${radius * 2} ${radius * 2}`} width={radius * 2} xmlns="http://www.w3.org/2000/svg">
+      <circle cx={radius} cy={radius} r={radius} fill={color} />
+    </svg>
+    {icon && <span className={styles.circleIcon}>{icon}</span>}
+  </div>
 );
 
 Circle.propTypes = {
@@ -37,6 +42,7 @@ const Terminal = ({ children, className, ...rest }) => {
   const [closed, setClosed] = useState(false);
   const [minimized, setMinimized] = useState(false);
   const [expanded, setExpanded] = useState(false);
+  const [showIcons, setShowIcons] = useState(false);
   const handleCloseClick = () => {
     setClosed(true);
   };
@@ -49,6 +55,12 @@ const Terminal = ({ children, className, ...rest }) => {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const handleTrafficLightsMouseEnter = () => {
+    setShowIcons(true);
+  };
+  const handleTrafficLightsMouseLeave = () => {
+    setShowIcons(false);
+  };
   const trafficLightStates = {
     [styles.closed]: closed,
     [styles.minimized]: minimized,
@@ -60,9 +72,11 @@ const Terminal = ({ children, className, ...rest }) => {
       {...rest}
     >
       <div className={styles.topBar}>
-        <Circle color="#ed645a" onClick={handleCloseClick} className={styles.closeCircle} />
-        <Circle color="#e1c14c" onClick={handleMinimizeClick} className={styles.minimizeCircle} />
-        <Circle color="#70bd3c" onClick={handleExpandClick} className={styles.expandCircle} />
+        <div className={styles.trafficLights} onMouseEnter={handleTrafficLightsMouseEnter} onMouseLeave={handleTrafficLightsMouseLeave}>
+          <Circle color="#ed645a" onClick={handleCloseClick} className={styles.closeCircle} icon={showIcons ? <FaTimes color="#951f16" size="12px" /> : null} />
+          <Circle color="#e1c14c" onClick={handleMinimizeClick} className={styles.minimizeCircle} icon={showIcons ? <FaMinus color="#ae8d34" size="12px" /> : null} />
+          <Circle color="#70bd3c" onClick={handleExpandClick} className={styles.expandCircle} icon={showIcons ? <FaPlus color="#265a15" size="12px" /> : null} />
+        </div>
       </div>
       <div className={styles.terminalBody}>
         {children}
